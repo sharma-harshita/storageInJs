df77d42280708f1fa6744b845fdaa9cd935242fc
harshita.sharma <harshita.sharma@asksid.ai>
2021-11-23 17:13:42 +0530

    first

---
+++ index.js
@@ -0,0 +1,30 @@
+// console.log("Hello world!!!");
+
+
+
+var a = Symbol("12");
+var b = Symbol("12");
+console.log(a);
+console.log(b);
+
+console.log(a===b); //false
+
+// function handleSubmit (){
+//     //function definition will start here
+//     const username = document.getElementById("name").value;
+//     // console.log(username);
+//     localStorage.setItem("NAME",username);
+//     localStorage.setItem("salary",2000);
+//     localStorage.setItem("compnay","ABC");
+//     sessionStorage.setItem("name", username);
+// }
+
+
+// function handleShow(){
+//     const name = localStorage.getItem("salary")
+//     console.log(name);
+// }
+
+// function handleClear (){
+//     localStorage.clear()
+// }
